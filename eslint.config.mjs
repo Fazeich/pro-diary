5ac05f1da3fd0c4764964a1d5699557deccefe47
travnya <vladislavchenko@inbox.ru>
2024-09-23 15:27:44 +0300

    Настройка линтера

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,24 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    rules: {
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^react', '^@\\w', '^@?\\w', '^'],
+            ['^(@|store|api|hooks|utils|constants|icons)(/.*|$)'],
+            ['^\\./(?=.*/)(?!/?&)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^.+\\./styles.ts$'],
+          ],
+        },
+      ],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
